91d528155cc722319bc219d113bdbb56c7ee4109
Justin Nielsen <nielsenjustin97@gmail.com>
2019-06-07 20:17:45 -0600

    Initial Commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody rb;
+    float horizontal;
+    float vertical;
+    Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        movement = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        movement = new Vector3(horizontal, vertical, 0) * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.Translate(movement);
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    //Player obeject
+    private GameObject player;
+
+    //Movement speed of the enemy
+    public float speed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get a reference to the player through the player tag
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Find the distance to the player
+        Vector3 distanceToPlayerVector = player.transform.position - this.transform.position;
+        float distanceToPlayer = distanceToPlayerVector.magnitude;
+
+        //Look at the player
+        this.transform.LookAt(player.transform);
+
+        //Stop the enemy from getting too close to the player 
+        if(distanceToPlayer > 3)
+            this.transform.Translate(Vector3.forward * speed);
+    }
+}
