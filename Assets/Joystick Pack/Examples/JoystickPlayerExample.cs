14a1a1a4b520a94cf624fdad166b4b85875c77c7
Justin Nielsen <nielsenjustin97@gmail.com>
2019-06-22 20:28:36 -0600

    Added Joysticks and turning

---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public FloatingJoystick moveJoystick;
+    public FloatingJoystick lookJoystick;
+    public Rigidbody rb;
+    public GameObject enemy;
+    private Vector3 dir;
+
+    private void Start()
+    {
+        
+    }
+
+    public void FixedUpdate()
+    {
+        //Movement
+        Vector3 direction = Vector3.forward * moveJoystick.Vertical + Vector3.right * moveJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+        //Look     
+        if(lookJoystick.Direction != Vector2.zero)
+        {
+            dir = lookJoystick.Direction;
+        }
+
+        float heading = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, heading, 0f);
+    }
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,6 +6,8 @@ public class Controller : MonoBehaviour
 {
     //Player movement speed
     public float speed;
+    public FloatingJoystick moveJoystick;
+    public FloatingJoystick lookJoystick;
 
     [SerializeField]
     float movementSmoothing;
@@ -15,26 +17,29 @@ public class Controller : MonoBehaviour
     float vertical;
     Vector3 movement;
     Vector2 velocity;
+    Vector3 dir;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
         movement = Vector3.zero;
+        dir = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Get horizontal and vertical inputs
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-        //movement = new Vector3(horizontal, vertical, 0) * speed;
+        horizontal = moveJoystick.Horizontal;
+        vertical = moveJoystick.Vertical;
+        
     }
 
     private void FixedUpdate()
     {
         Move();
+        Look();
     }
 
     private void Move()
@@ -43,5 +48,16 @@ public class Controller : MonoBehaviour
         Vector2 targetVelocity = new Vector2((horizontal * speed * Time.deltaTime) * 10, (vertical * speed * Time.deltaTime) * 10);
         //Smooth the movement and apply it to the player
         rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+    }
+
+    private void Look()
+    {
+        if (lookJoystick.Direction != Vector2.zero)
+        {
+            dir = lookJoystick.Direction;
+        }
+
+        float heading = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, -heading);
     }
 }
